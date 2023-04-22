a6dc528668225cd9efdcb93bf20fbe58065d8888
Michael Zhao <michael.yj.zhao@gmail.com>
2023-04-22 14:00:06 +1000

    Added database schema and seed

--- database/db.js
+++ database/db.js
@@ -1,1 +1,7 @@
-// TODO
+const pg = require('pg')
+
+const db = new pg.Pool({
+  database: 'scavenger_hunt'
+})
+
+module.exports = db
--- database/schema.sql
+++ database/schema.sql
@@ -1,1 +1,56 @@
--- TODO
+DROP TABLE IF EXISTS answers;
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS quizzes;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  email TEXT NOT NULL UNIQUE,
+  password_hash TEXT NOT NULL,
+  first_name TEXT,
+  last_name TEXT
+);
+
+CREATE TABLE quizzes (
+  id SERIAL PRIMARY KEY,
+  user_id INT,
+  quiz TEXT,
+  prompt TEXT,
+  difficulty TEXT,
+  context TEXT,
+  CONSTRAINT fk_quizzes_users
+    FOREIGN KEY (user_id)
+    REFERENCES users(id)
+);
+
+CREATE TABLE questions (
+  id SERIAL PRIMARY KEY,
+  quiz_id INT,
+  question TEXT,
+  choices TEXT,
+  correct_answer TEXT,
+  CONSTRAINT fk_questions_quizzes
+    FOREIGN KEY (quiz_id)
+    REFERENCES quizzes(id)
+);
+
+CREATE TABLE answers (
+  id SERIAL PRIMARY KEY,
+  user_id INT, 
+  quiz_id INT,
+  question_id INT,
+  user_answer TEXT,
+  correct_answer TEXT,
+  correct INT,
+  CONSTRAINT fk_answers_users
+    FOREIGN KEY (user_id)
+    REFERENCES users(id),
+  CONSTRAINT fk_answers_quizzes
+    FOREIGN KEY (quiz_id)
+    REFERENCES quizzes(id),
+  CONSTRAINT fk_answers_questions
+    FOREIGN KEY (question_id)
+    REFERENCES questions(id)
+);
+
+
