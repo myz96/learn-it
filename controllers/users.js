f9b0d155d04da15d66611d89b6decfb4510b73d2
David Kuperman <kuperman@192-168-1-108.tpgi.com.au>
2023-04-24 20:44:45 +1000

    updating sign up in users

--- controllers/users.js
+++ controllers/users.js
@@ -1,42 +1,48 @@
 const express = require('express')
 const bcrypt = require('bcrypt')
-const createUser = require('../models/user')
+const { createUser } = require('../models/user')
 const router = express.Router()
 
 const generateHash = (password) => bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
 
-router.post('/', (req, res, next) => {
-  const { first_name, last_name, email, password } = Object.entries(req.body).reduce((o, [k, v]) => {
-    o[k] = v.trim()
-    return o
-  }, {})
+router.post('/', async (req, res, next) => {
+    try{
+    const { first_name, last_name, email, password } = Object.entries(req.body).reduce((o, [k, v]) => {
+        o[k] = v.trim()
+        return o
+    }, {})
 
-  if (!first_name) {
-    const customError = new Error("Please enter your First Name")
-    customError.status = 400
-    return next(customError)
-  }
+    if (!first_name) {
+        const customError = new Error("Please enter your First Name")
+        customError.status = 400
+        return next(customError)
+    }
 
-  if (!last_name) {
-    const customError = new Error("Please enter your Last Name")
-    customError.status = 400
-    return next(customError)  }
+    if (!last_name) {
+        const customError = new Error("Please enter your Last Name")
+        customError.status = 400
+        return next(customError)
+    }
 
-  if (!email) {
-    const customError = new Error("Please enter a valid email address")
-    customError.status = 400
-    return next(customError)  }
+    if (!email) {
+        const customError = new Error("Please enter a valid email address")
+        customError.status = 400
+        return next(customError)
+    }
 
-  if (!password) {
-    const customError = new Error("Please your password")
-    customError.status = 400
-    return next(customError) 
-  }
+    if (!password) {
+        const customError = new Error("Please enter a password")
+        customError.status = 400
+        return next(customError)
+    }
 
-  const password_hash = generateHash(password)
+    const password_hash = generateHash(password)
 
-  createUser(first_name, last_name, email, password_hash)
-
+    const result = await createUser(first_name, last_name, email, password_hash)
+    return res.status(200).json(result[0])
+    } catch(error){
+        return next(error)
+    }
 })
 
 module.exports = router
