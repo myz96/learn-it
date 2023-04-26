a947c405d0dbd6892e092623d97090e7c728a640
Michael Zhao <michael.yj.zhao@gmail.com>
2023-04-26 22:52:46 +1000

    Updated header UI

--- client/js/initialise.js
+++ client/js/initialise.js
@@ -1,9 +1,5 @@
 import browseQuizzes from "./components/browseQuizzes.js";
-import createQuiz from "./components/createQuiz.js";
 import renderHeader from "./components/renderHeader.js";
-import renderLoadingPage from "./components/renderLoading.js";
-import renderProgressPage from "./components/renderProgress.js";
-import renderQuiz from "./components/renderQuiz.js";
 
 const initialise = async () => {
     try {
@@ -23,26 +19,3 @@ const initialise = async () => {
 }
 
 initialise()
-
-// browseQuizzes()
-// renderProgressPage()
-// renderLoadingPage()
-// createQuiz()
-// renderQuiz()
-
-// const initialise = async () => {
-//     try {
-//         const res = await axios.get('/api/session')
-//         const data = res.data
-
-//         if ('user' in data){
-//             renderQuiz()
-//         } else {
-//             window.location = '/signup.html'
-//         }
-//     } catch (error) {
-//         console.log(error)
-//     }
-// }
-
-// initialise()
