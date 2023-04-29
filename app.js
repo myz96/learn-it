3a4511e35d7011273398e1ed1616f18635c67faa
shiam <shiamuddulla29@hotmail.com>
2023-04-29 11:19:42 +1000

    Added deployment config

--- app.js
+++ app.js
@@ -29,9 +29,49 @@ app.use(session({
   saveUninitialized: false, // if true , all requests (even if nothing happens) will create session in db - used to track anon users. if false, will only save if create/add data. usually want false.  
   store: new pgSession({
     pool: db,
-    createTableIfMissing: true
+    createTableIfMissing: true, 
   })
 }))
+
+db.query(`DROP TABLE IF EXISTS answers;
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS quizzes;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  email TEXT NOT NULL UNIQUE,
+  password_hash TEXT NOT NULL,
+  first_name TEXT,
+  last_name TEXT
+);
+
+CREATE TABLE quizzes (
+  id SERIAL PRIMARY KEY,
+  user_id INT,
+  quiz TEXT,
+  title TEXT,
+  topic TEXT,
+  difficulty TEXT,
+  context TEXT,
+  image_url TEXT,
+  CONSTRAINT fk_quizzes_users
+    FOREIGN KEY (user_id)
+    REFERENCES users(id)
+);
+
+CREATE TABLE questions (
+  id SERIAL PRIMARY KEY,
+  user_id INT, 
+  quiz_id INT,
+  question TEXT,
+  user_answer TEXT,
+  correct TEXT,
+  CONSTRAINT fk_questions_users
+    FOREIGN KEY (user_id)
+    REFERENCES users(id)
+);
+`)
 
 app.use(logger)
 
